0ad306b1774d9045e22cb49eb3604f4c3c75036d
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-09-22 13:31:10 +0000

    Add react todo files

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+      <header className="bg-gray-900 text-white">
+        <section className="mx-auto flex justify-between py-5 px-5">
+          <h1 className="text-xl">Todo List</h1>
+          <p>&copy; React Todo List</p>
+        </section>
+      </header>
+  );
+}
+
+export default Footer;
